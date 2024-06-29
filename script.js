6c54ce0466c537d7228962471f4c03b663c8f3bf
JulioV <julio.valenzuela20@inacapmail.cl>
2024-06-29 17:15:58 -0400

    Más botones

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-var modal = document.getElementById("modal");
-var btn = document.querySelector(".nav li a[href='#']");
-var span = document.getElementsByClassName("close")[0];
+// Obtener referencias a los elementos del DOM
+const modalCliente = document.getElementById("modal-cliente")
+const btnAgregarCliente = document.getElementById("btnAgregarCliente")
+const modalReserva = document.getElementById("modal-reserva")
+const btnAgregarReserva = document.getElementById("btnAgregarReserva")
+const spans = document.getElementsByClassName("close") // Asume que hay un span.close para cada modal
 
-btn.onclick = function() {
-    modal.style.display = "block";
+// Función para abrir un modal específico
+const abrirModal = (modal) => {
+    modal.style.display = "block"
 }
 
-span.onclick = function() {
-    modal.style.display = "none";
+// Función para cerrar modales
+const cerrarModal = (modal) => {
+    modal.style.display = "none"
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+// Asignar evento onclick a los botones para abrir los modales
+btnAgregarCliente.onclick = () => {
+    abrirModal(modalCliente)
+}
+
+btnAgregarReserva.onclick = () => {
+    abrirModal(modalReserva)
+}
+
+// Asignar evento onclick a los spans para cerrar los modales
+for (let i = 0; i < spans.length; i++) {
+    spans[i].onclick = () => {
+        cerrarModal(modalCliente);
+        cerrarModal(modalReserva);
+    }
+}
+
+// Cerrar modales al hacer clic fuera de ellos
+window.onclick = (event) => {
+    if (event.target == modalCliente) {
+        cerrarModal(modalCliente)
+    } else if (event.target == modalReserva) {
+        cerrarModal(modalReserva)
     }
 }
